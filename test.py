fc0f2216a209b9f46904a3fba0459b0b04fe18f7
TharushaAMCC <49670773+TharushaAMCC@users.noreply.github.com>
2020-11-23 00:59:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import requests
-
-payload = {"Authorization":"JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzNkYjU0M2E2MWE1NTA1MjQxZWU4YzciLCJtb2JpbGUiOiI3NjMzNzI1MjYiLCJrZXkiOiI1NDI5Njk5IiwiaWF0IjoxNjA1OTYzMDcwfQ.6isj1TU3LJ5PaHySpTFxiHWpTrlDW2iBzvTGzeuXiSI","X-Unity-Version":"2018.3.0f2","Host":"http://megarun.dialog.lk"}
-r = requests.post("https://megarun.dialog.lk/api/v2/play", data=payload)
-print(r.text)
+except ImportError:
+    print('%s Requests isn\'t installed, installing now.')
+    os.system('pip3 install requests')
+    print('%s Requests has been installed.')
+    os.system('clear')
+    import requests
+    header = {"Host": "megarun.dialog.lk",
+              "Authorization": "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzNkYjU0M2E2MWE1NTA1MjQxZWU4YzciLCJtb2JpbGUiOiI3NjMzNzI1MjYiLCJrZXkiOiI1NDI5Njk5IiwiaWF0IjoxNjA1OTYzMDcwfQ.6isj1TU3LJ5PaHySpTFxiHWpTrlDW2iBzvTGzeuXiSI", "X-Unity-Version": "2018.3.0f2"}
+r = requests.post(url, headers=headers)
